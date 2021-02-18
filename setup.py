a1caf90f48a9db6969da883ce05e08520927e3b9
saby2184 <66889635+saby2184@users.noreply.github.com>
2021-02-18 12:24:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
   description = 'Text PreProcessing libraries',   # Give a short description about your library
   author = 'Saby',                   # Type in your name
   author_email = 'saby2184@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/saby2184/RnD/tree/main/textpreproc',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  url = 'https://github.com/saby2184/TPP',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/saby2184/TPP/archive/v0.01.tar.gz',    # I explain this later on
   keywords = ['NOISE', 'WHITESPACE', 'SMALLCASE'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           're',
